3b6b6602a1098c05fcae633e42cf7fb7014515f2
dukerspace <montolsaklor@gmail.com>
2024-02-12 16:58:58 +0700

    Update css

--- src/Component/Footer/Footer.tsx
+++ src/Component/Footer/Footer.tsx
@@ -3,7 +3,8 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <div className="footer">
-      193-195 Lake Rajada Office Complex,Ratchadapisek road, Khlong Toei, Bangkok
+      <p>Drivehub Co.,Ltd</p>
+      <p>193-195 Lake Rajada Office Complex,Ratchadapisek road, Khlong Toei, Bangkok</p>
     </div>
   )
 }
